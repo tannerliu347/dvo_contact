739d02722d9c2434fbf0b8f99fd94f477a19df48
Tingjun-Li <tingjunl@umich.edu>
2021-12-06 21:57:26 +0000

    Added point_selection and yaml

--- src/experiments/rgbd_test.cpp
+++ src/experiments/rgbd_test.cpp
@@ -1,12 +1,21 @@
 #include "rgbd_image.h"
 #include "TUM_loader.h"
+#include "yaml-cpp/yaml.h"
 
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 #include <iostream>
 
 int main() {
-    dvo::TUMLoader tum_loader("/home/tannerliu/dvo_contact/dataset/rgbd_dataset_freiburg1_xyz/");
+    char resolved_path[PATH_MAX];
+    realpath("../", resolved_path);
+    std::cout << resolved_path << std::endl;
+    YAML::Node config_setting = YAML::LoadFile(std::string(resolved_path) + "/config/config.yaml");
+    
+    std::string image_load_path = config_setting["dvo"]["image_load_path"] ? config_setting["dvo"]["image_load_path"].as<std::string>() 
+                                                                : "/home/tingjun/code/dvo_contact/dataset/";
+
+    dvo::TUMLoader tum_loader(image_load_path);
     // auto cur = tum_loader.getNext();
     while (tum_loader.hasNext()) {
         auto cur = tum_loader.getNext();
